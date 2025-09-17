6b9f086e454fed7ac574460dcd6d6f136832ae89
KNJS0418 <klightjs@gmail.com>
2025-09-17 22:23:09 +0700

    assignment complete

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let productPrice = 27000;
+let discount = 0.25;
+let finalPrice = productPrice - (productPrice * discount);
+console.log(finalPrice);
